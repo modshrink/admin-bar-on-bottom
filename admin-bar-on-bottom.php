620c23fe195151c0c4b3dbd6627f7f883eb702d4
mayoibi <mayoibi@modshrink.com>
2014-01-15 22:03:48 +0900

    Create plugin meta information.

---
+++ admin-bar-on-bottom.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Admin Bar on Bottom
+Plugin URI: https://github.com/modshrink/admin-bar-on-bottom
+Description: Move admin bar to bottom.
+Version: 0.0.1
+Author: mayoibi
+Author URI: http://www.modshrink.com/
+*/
