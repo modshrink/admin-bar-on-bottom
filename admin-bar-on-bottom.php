72d5178d6a27bf95e5a87430db29599e101cf9bb
mayoibi <mayoibi@modshrink.com>
2014-01-16 15:46:35 +0900

    Add action hooks.

--- admin-bar-on-bottom.php
+++ admin-bar-on-bottom.php
@@ -7,3 +7,17 @@ Version: 0.0.1
 Author: mayoibi
 Author URI: http://www.modshrink.com/
 */
+
+add_action( 'wp_enqueue_scripts', 'admin_bar_style_init' );
+add_action( 'get_header','remove_admin_bar_css' );
+
+function admin_bar_style_init() {
+     wp_register_style( 'adminBarStyleSheet', plugins_url('css/view.css', __FILE__) );
+     wp_enqueue_style( 'adminBarStyleSheet' );
+}
+
+function remove_admin_bar_css() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+
+}
+ 
