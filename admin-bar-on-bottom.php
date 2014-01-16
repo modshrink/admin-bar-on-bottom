40e73428c0a03247cf8808cb5f5c7fe6cd42a98e
mayoibi <mayoibi@modshrink.com>
2014-01-16 21:46:37 +0900

    Add load fold menu javascript.

--- admin-bar-on-bottom.php
+++ admin-bar-on-bottom.php
@@ -10,14 +10,33 @@ Author URI: http://www.modshrink.com/
 
 add_action( 'wp_enqueue_scripts', 'admin_bar_style_init' );
 add_action( 'get_header','remove_admin_bar_css' );
+add_action( 'add_admin_bar_menus', 'parent_menu_item', 10 );
 
 function admin_bar_style_init() {
      wp_register_style( 'adminBarStyleSheet', plugins_url('css/view.css', __FILE__) );
+     wp_register_script( 'adminBarJavaScript', plugins_url('js/admin-bar-on-bottom.js', __FILE__), array('jquery') );
      wp_enqueue_style( 'adminBarStyleSheet' );
+     wp_enqueue_script( 'adminBarJavaScript' );
 }
 
 function remove_admin_bar_css() {
     remove_action('wp_head', '_admin_bar_bump_cb');
 
 }
- 
+
+function parent_menu_item() {
+    global $wp_admin_bar;
+    $wp_admin_bar->add_menu( 
+        array( 
+            'id'        => 'parent_id_name',
+            'meta'     => array( 'html' => '',
+                                 'class' => 'admin-bar-fold',
+                                 'onclick' => '',
+                                 'target' => '',
+                                 'title' => ''
+                                ),
+            'title'     => __( '', '' ),
+            'href'      => '#'
+        ) 
+    );
+}
